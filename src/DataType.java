d61f607ebd7ee4a4e5cb78939323ceef8bd0953e
Admin <amolujagare@gmail.com>
2025-01-16 11:29:26 +0530

    Initial commit

---
+++ src/DataType.java
@@ -0,0 +1,23 @@
+public class DataType {
+
+    public static void main(String[] args) {
+
+        byte b = -128;
+        short s = 32767;
+        int i = 1656565656;
+        long l = 1878787878787877878l ;
+
+        float f = 23.56f;
+        double d = 78.56;
+
+        char ch = '#';
+        boolean b1 = true;
+
+        char ch1 = 103;
+        System.out.println(ch1);
+
+        int i1 = 't' ;
+        System.out.println(i1);
+
+    }
+}
